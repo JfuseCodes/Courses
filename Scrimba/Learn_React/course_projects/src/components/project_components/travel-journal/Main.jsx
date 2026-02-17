7d4f768a6e2047fa300c40d209a79138be867532
Joshua Mendoza <93098953+JfuseCodes@users.noreply.github.com>
2026-02-16 21:17:52 -0500

    Completed Travel Journal

--- Scrimba/Learn_React/course_projects/src/components/project_components/travel-journal/Main.jsx
+++ Scrimba/Learn_React/course_projects/src/components/project_components/travel-journal/Main.jsx
@@ -1,13 +1,26 @@
 import Entry from "./sub-components/Entry"
+import data from './../travel-journal/sub-components/data.js'
 
 const Main = () => {
+
+    
+    const travelEntries = data.map((entry) => {
+        return <Entry 
+                id={entry.id}
+                img={entry.img}
+                title= {entry.title}
+                country= {entry.country}
+                googleMapsLink = {entry.googleMapsLink}
+                dates = {entry.dates}
+                text = {entry.text}
+        />
+    })
 
     return (
         <main className='tj--main-container'>
             <div className='tj--main-content'>
-                <Entry />
+                {travelEntries}
             </div>
-            
         </main>
     )
 }
--- Scrimba/Learn_React/course_projects/src/components/project_components/travel-journal/sub-components/Entry.jsx
+++ Scrimba/Learn_React/course_projects/src/components/project_components/travel-journal/sub-components/Entry.jsx
@@ -1,15 +1,12 @@
-import mtFujiPhoto from '../../../../assets/travel-journal/images/mtfuji.png'
 import mapPin from '../../../../assets/travel-journal/map-pin.png'
 
-const Entry = () => {
 
-
-    const mapsLink = "Google Maps Link:https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+const Entry = (props) => {
 
     return (
         <><div className='tj--entry'>
             <div className='tj--entry-image-container'>
-                <img src={mtFujiPhoto} alt='picture of mount fuji'/>
+                <img src={props.img.src} alt={props.img.alt}/>
             </div>
             <div className='tj--entry-details tj--primary-dark'>
                 <div className='ed-header'>
@@ -18,21 +15,21 @@ const Entry = () => {
                             
                             <div className='ed-location--name'>
                                 <img src={mapPin} about='location symbol'/>
-                                <span className= 'location--country fs-10'>JAPAN</span>
+                                <span className= 'location--country fs-10'>{props.country}</span>
                             </div>
                             <div className='ed-location--link fs-10'>
-                                <a className='location--google-link' href={mapsLink}>View on Google Maps</a>
+                                <a className='location--google-link' href={props.googleMapsLink}>View on Google Maps</a>
                             </div>
                         </div>
                         <div className='ed-location--name-landmark fs-25 fw-700'>
-                            <span>Mount Fuji</span>
+                            <span>{props.title}</span>
                         </div>
                     </div>
                 </div>
                 <div className='tj--ed-content'>
-                    <div className='ed-content--timeline fs-10 fw-700 mt-9'>12 Jan, 2021 - 24 Jan, 2021</div>
+                    <div className='ed-content--timeline fs-10 fw-700 mt-9'>{props.dates}</div>
                     <div className='ed-content--description fs-10 lh-150 fw-300 mt-9'>
-                        Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                        {props.text}
                     </div>
                 </div>
             </div>
